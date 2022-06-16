ad8110d8fc83524134b9ba5083391214ecfccfaf
LLL762 <lamirallaurent@gmail.com>
2022-06-16 16:56:34 +0200

    Switch css class crash test.

--- src/main/java/com/example/musicplayer/HelloApplication.java
+++ src/main/java/com/example/musicplayer/HelloApplication.java
@@ -16,8 +16,11 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+
+        scene.getStylesheets().add(HelloApplication.class.getResource("/style/home.css").toExternalForm());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
     }
 }
